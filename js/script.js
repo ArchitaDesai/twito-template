f94a556e00af4ff5a56db99f55eb995831888425
kavan <kavanpancholi@gmail.com>
2017-01-10 02:26:40 +0530

    javascript: sliding cards in user page

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
 $(function(){
-    var windowH = $(window).height();
-    var body = $('body').height();
-    if(windowH > body) {
-        $('body').css({'height':($(window).height())+'px'});
-    }
-    $(window).resize(function(){
-        var windowH = $(window).height();
-        var body = $('#wrapper').height();
-        var differenceH = windowH - body;
-        var newH = body + differenceH;
-        var truecontentH = $('#truecontent').height();
-        if(windowH > truecontentH) {
-            $('#wrapper').css('height', (newH)+'px');
-        }
-    })
+    $('#card-1 .task-type-container a').click(function () {
+        var type = $(this).data('type');
+        $('#card-1').removeClass('animated').addClass('animated fadeInRight');
+        $('#card-2').removeClass('hidden animated').addClass('animated fadeInLeft');
+    });
+
+    $('#card-2 a').click(function () {
+        var type = $(this).data('type');
+        $('#card-1 .task-type-container > div').removeClass('col-md-6');
+        $('#card-1').removeClass('animated').addClass('animated fadeInRight');
+        $('#card-2').removeClass('animated').addClass('animated fadeInRight');
+        $('#card-3').removeClass('hidden animated').addClass('animated fadeInLeft');
+    });
+
+    $('#card-3 a').click(function () {
+        var type = $(this).data('type');
+        $('#card-1').removeClass('animated').addClass('animated fadeInRight');
+        $('#card-2').removeClass('animated').addClass('animated fadeInRight');
+        $('#card-3').removeClass('animated').addClass('animated fadeInRight');
+        $('#card-4').removeClass('hidden animated').addClass('animated fadeInLeft');
+    });
 });
