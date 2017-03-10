0fd8c94dd9f1624878187980d803b7e9e511dee3
Vatsal Parekh <vatsalparekh@outlook.com>
2017-03-10 12:36:30 +0530

    Fix from kavan

--- js/script.js
+++ js/script.js
@@ -1,20 +1,37 @@
-var curr_step,max_step=4;
-$(function(){
+var curr_step, max_step = 4, wrapper = $('#wrapper');
+$(function () {
     $('a[data-type]').click(function () {
         var type = $(this).data('type');
         var step = $(this).parents('.card-container').data('step');
         $(this).parents('.card-container').removeClass('animated').addClass('animated fadeInLeft');
-        if(step==curr_step){
-            $('div[data-step="'+(curr_step+1)+'"]').addClass('hidden');
+        if (step == curr_step) {
+            $('div[data-step="' + (curr_step + 1) + '"]').addClass('hidden');
             $('.form').trigger('reset').find('label').removeClass('active');
         }
-        if(step<curr_step){
-            for(var i=step;i<max_step;i++){
-                $('div[data-step="'+(i+1)+'"]').addClass('hidden');
+        if (step < curr_step) {
+            for (var i = step; i < max_step; i++) {
+                $('div[data-step="' + (i + 1) + '"]').addClass('hidden');
             }
             $('.form').trigger('reset').find('label').removeClass('active');
         }
-        $("div[data-parent='"+type+"']").removeClass('hidden animated').addClass('animated fadeInLeft');
-        curr_step=step;
+        if (step >= 2) {
+            $(".col-centered").find('.col-md-6').removeClass('col-md-6').addClass('col-md-12');
+        } else {
+            $(".col-centered").find('.col-md-12').removeClass('col-md-12').addClass('col-md-6');
+        }
+        $("div[data-parent='" + type + "']").removeClass('hidden animated').addClass('animated fadeInLeft');
+        resize();
+        curr_step = step;
+    });
+    resize();
+    $(window).resize(function(){
+        resize();
     });
 });
+function resize(){
+    var windowH = $(window).height();
+    var wrapperH = wrapper.height();
+    if (windowH > wrapperH) {
+        wrapper.css({'height': ($(window).height()) + 'px'});
+    }
+}
